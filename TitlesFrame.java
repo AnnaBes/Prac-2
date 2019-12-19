e5023942135c168a22cdd50d17df89ecba58d656
AnnaBes <58949966+AnnaBes@users.noreply.github.com>
2019-12-19 23:14:47 +0200

    Документация

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -6,12 +6,16 @@ import javax.swing.JFrame;
 // Decompiled by Procyon v0.5.36
 // 
 
-public class TitlesFrame extends JFrame
+public class TitlesFrame extends JFrame 
 {
+    
+    /** 
+     * @return 
+     * разрешение консольного окна, оглавление, вызов TitlesPanel
+     */
     public TitlesFrame() {
         this.initUI();
     }
-
     private void initUI() {
         this.setTitle("\u041a\u0440\u0438\u0432\u044b\u0435 \u0444\u0438\u0433\u0443\u0440\u044b");
         this.setDefaultCloseOperation(3);
@@ -19,8 +23,13 @@ public class TitlesFrame extends JFrame
         this.setSize(350, 350);
         this.setLocationRelativeTo(null);
     }
-
-    public static void main(final String[] args) {
+    
+    
+    /** 
+     * @param Runnable(
+     * main
+     */
+    public static void main(final String[] args) { /** ddd */
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
